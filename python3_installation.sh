266114bee2be54182ebf4fac5ca2524f0e0c0bab
Philip Xu <philipxjm1@gmail.com>
2018-06-05 12:59:24 -0400

    Create python3_installation.sh

---
+++ python3_installation.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+cd ~
+mkdir tmp
+cd tmp
+wget https://www.python.org/ftp/python/3.6.2/Python-3.6.2.tgz
+tar zxvf Python-3.6.2.tgz 
+cd Python-3.6.2 
+./configure --prefix=$HOME/opt/python-3.6.2
+make
+make install
